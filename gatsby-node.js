f2f6e74e78796e4e599fefe3d39edefc60bb802f
Richiely Paiva <richiely.paiva@auvo.com.br>
2021-11-15 17:40:02 -0300

    log

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,7 +5,6 @@ const { createFilePath } = require("gatsby-source-filesystem")
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
 
-  debugger
   if (node.internal.type === 'MarkdownRemark') {
     const slug = createFilePath({ node, getNode, basePath: 'content' })
     createNodeField({
@@ -54,25 +53,28 @@ exports.createPages = async ({ graphql, actions }) => {
   const allPages = content.data.pages.edges;
 
   allPosts.forEach((node) => {
-    if (node.frontmatter.published) {
-      createPage({
-        path: node.fields.slug,
-        component: path.resolve(`./src/templates/Post.js`),
-        context: {
-          slug: node.fields.slug,
-        },
-      })
-    }
+    console.log('post', node)
+    //if (node.frontmatter.published) {
+    // createPage({
+    //   path: node.fields.slug,
+    //   component: path.resolve(`./src/templates/Post.js`),
+    //   context: {
+    //     slug: node.fields.slug,
+    //   },
+    // })
+    //}
   })
 
   allPages.forEach((node) => {
-    createPage({
-      path: node.fields.slug,
-      component: path.resolve(`./src/templates/Page.js`),
-      context: {
-        slug: node.fields.slug,
-      },
-    })
+    console.log('page', node)
+
+    // createPage({
+    //   path: node.fields.slug,
+    //   component: path.resolve(`./src/templates/Page.js`),
+    //   context: {
+    //     slug: node.fields.slug,
+    //   },
+    // })
   })
 
 }
